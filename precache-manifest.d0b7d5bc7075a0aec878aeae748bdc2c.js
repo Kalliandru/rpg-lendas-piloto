03ccfdc97cdb4dcef671350f8ba674b1d8e054f1
Kalliandru <kalliandru@gmail.com>
2020-10-06 20:52:33 -0300

    Updates

---
+++ precache-manifest.d0b7d5bc7075a0aec878aeae748bdc2c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be210ead1710b580d7355c1a6cd4ca9",
+    "url": "/Kalliandru/rpg-lendas-piloto.git/index.html"
+  },
+  {
+    "revision": "f0610d074bfaf315a3ab",
+    "url": "/Kalliandru/rpg-lendas-piloto.git/static/css/main.b325f374.chunk.css"
+  },
+  {
+    "revision": "5796b7a67311ce63cc4d",
+    "url": "/Kalliandru/rpg-lendas-piloto.git/static/js/2.7394a585.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kalliandru/rpg-lendas-piloto.git/static/js/2.7394a585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0610d074bfaf315a3ab",
+    "url": "/Kalliandru/rpg-lendas-piloto.git/static/js/main.bfafdfd2.chunk.js"
+  },
+  {
+    "revision": "f5cd027783fdde68a56d",
+    "url": "/Kalliandru/rpg-lendas-piloto.git/static/js/runtime-main.53d8173d.js"
+  },
+  {
+    "revision": "cb4cba903f084a16b789cc89fddaf19f",
+    "url": "/Kalliandru/rpg-lendas-piloto.git/static/media/bg Lendas.cb4cba90.jpg"
+  },
+  {
+    "revision": "4a8c23476a7c20c5bee2a752a6f96e9e",
+    "url": "/Kalliandru/rpg-lendas-piloto.git/static/media/insta.4a8c2347.png"
+  }
+]);
